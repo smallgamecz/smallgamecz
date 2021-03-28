adfac32a6934c5ea704ee7df24040a9dce3ce5c1
Daniel Rataj <daniel.rataj@mall.cz>
2021-03-28 23:06:08 +0200

    Remove link to github

--- panel/src/pages/Homepage.vue
+++ panel/src/pages/Homepage.vue
@@ -1,10 +1,5 @@
 <template>
   <q-page>
-    <div class="text-right q-ma-md">
-      <a href="https://github.com/danielrataj/smallgamecz" title="GitHub" target="_blank">
-        <img src="~/assets/icons/github.svg" width="20" height="20" />
-      </a>
-    </div>
     <div class="fixed-center text-center">
       <div>
         <img src="/logo/favicon_1024_1024.png" alt="logo" width="300">
